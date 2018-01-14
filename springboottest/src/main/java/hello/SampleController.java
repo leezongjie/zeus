84c5a5bc111a7baae98a242bf2ba5151b31ece4e
leezongjie <leezongjie@163.com>
2018-01-14 21:49:08 +0800

    Add files via upload

---
+++ springboottest/src/main/java/hello/SampleController.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "sample")
+@EnableAutoConfiguration
+public class SampleController {
+    private Logger logger= LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/aaa")
+    @ResponseBody
+    String home() {
+        logger.info("receive message");
+        return "Hello World!12";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
