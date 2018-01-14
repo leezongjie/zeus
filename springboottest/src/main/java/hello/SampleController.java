5469379e65963eff0cb80fa6414ab96cda9636cc
leezongjie <leezongjie@163.com>
2018-01-14 22:35:11 +0800

    commit

--- springboottest/src/main/java/hello/SampleController.java
+++ springboottest/src/main/java/hello/SampleController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "sample")
 @EnableAutoConfiguration
+@ComponentScan
 public class SampleController {
     private Logger logger= LoggerFactory.getLogger(getClass());
 
